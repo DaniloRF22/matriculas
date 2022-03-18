e7e36ef6590b24993a0016beeca0c0b14553aa41
aldoorivera <ag_osorior@unicah.edu>
2022-03-18 01:50:00 -0600

    Ruta

--- routes/api/v1/v1.js
+++ routes/api/v1/v1.js
@@ -4,11 +4,15 @@ const router = express.Router();
 const estudiantesRoutes = require('./estudiantes/estudiantes');
 const aulasRoutes = require('./aulas/aulas');
 const seccionesRoutes =require('./secciones/secciones');
+const gradosRoutes =require('./grados/grados');
+
 
 const { verifyApiHeaderToken} = require('./headerVerifyMiddleware');
 
 router.use('/estudiantes',verifyApiHeaderToken, estudiantesRoutes);
 router.use('/aulas',verifyApiHeaderToken,aulasRoutes);
-router.use('/secciones',verifyApiHeaderToken,seccionesRoutes)
+router.use('/secciones',verifyApiHeaderToken,seccionesRoutes);
+router.use('/grados',verifyApiHeaderToken,gradosRoutes);
+
 
 module.exports= router;
